c248301951cc1266136e2ab7b6c6f5cc54d86164
Sebastian Thiel <byronimo@gmail.com>
2015-04-26 19:08:01 +0200

    feat(deploy): simple osx deploy script
    * added simple script to build tar archive with all debug/release
      binaries.
    * slightly improved docker script, even though it would need additional
      work. For now, I use the cloud VM anyway

--- .docker-build-cli.sh
+++ .docker-build-cli.sh
@@ -3,4 +3,4 @@
 # make cargo-cli ARGS="build --release"
 # DEBUG: only try to build a small CLI for now
 make discovery1-cli-cargo ARGS="build --release"
-find gen -executable -type f -path "*/release/*" -not \( -name "*.*" -or -name "*script*" \) | xargs cp -v /build-result
+find gen -executable -type f -path "*/release/*" -not \( -name "*.*" -or -name "*script*" \) | xargs -J % cp -v % /build-result
