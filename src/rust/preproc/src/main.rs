8cb73d66c245f0f15bbe5c697f92c0351c41fd38
Sebastian Thiel <sthiel@thoughtworks.com>
2019-07-05 14:50:05 +0800

    Add simple preprocessor to handle descriptions interpreted by `cargo doc`
    This seemed easiest, especially since I also am the author of
    pulldown-cmark-to-cmark :D.
    
    Funny how things fit together sometimes. And so much better than
    if I would have tried the same in pure python.

---
+++ src/rust/preproc/src/main.rs
@@ -0,0 +1,36 @@
+extern crate pulldown_cmark;
+extern crate pulldown_cmark_to_cmark;
+
+use pulldown_cmark::Parser;
+use pulldown_cmark_to_cmark::fmt::cmark;
+use std::io::{self, Read, Write};
+
+fn main() {
+    let md = {
+        let mut buf = String::with_capacity(2048);
+        io::stdin().read_to_string(&mut buf).unwrap();
+        buf
+    };
+
+    let mut output = String::with_capacity(2048);
+
+    cmark(
+        Parser::new_ext(&md, pulldown_cmark::Options::all()).map(|e| {
+            use pulldown_cmark::Event::*;
+            match e {
+                Start(ref tag) => {
+                    use pulldown_cmark::Tag::*;
+                    match tag {
+                        CodeBlock(code) => Start(CodeBlock(format!("ignore{}", code).into())),
+                        _ => e,
+                    }
+                }
+                _ => e,
+            }
+        }),
+        &mut output,
+        None,
+    )
+    .unwrap();
+    io::stdout().write_all(&output.as_bytes()).unwrap();
+}
