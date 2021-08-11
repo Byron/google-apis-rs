732384aa36ee90ee3af5a5ced263659d90466d0f
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-08-11 09:38:31 +0800

    bump patch level to 2.0.7; regen drive3

--- gen/drive3/src/api.rs
+++ gen/drive3/src/api.rs
@@ -156,7 +156,7 @@ impl<'a, > DriveHub<> {
         DriveHub {
             client,
             auth: authenticator,
-            _user_agent: "google-api-rust-client/2.0.6".to_string(),
+            _user_agent: "google-api-rust-client/2.0.7".to_string(),
             _base_url: "https://www.googleapis.com/drive/v3/".to_string(),
             _root_url: "https://www.googleapis.com/".to_string(),
         }
@@ -194,7 +194,7 @@ impl<'a, > DriveHub<> {
     }
 
     /// Set the user-agent header field to use in all requests to the server.
-    /// It defaults to `google-api-rust-client/2.0.6`.
+    /// It defaults to `google-api-rust-client/2.0.7`.
     ///
     /// Returns the previously set user-agent.
     pub fn user_agent(&mut self, agent_name: String) -> String {
