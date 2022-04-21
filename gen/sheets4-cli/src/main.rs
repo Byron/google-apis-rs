3c5c971b3bc15642759f9dc81da0608a52be9a27
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-04-22 07:20:32 +0800

    regen sheets 4 to prepare for release

--- gen/sheets4-cli/src/main.rs
+++ gen/sheets4-cli/src/main.rs
@@ -1613,7 +1613,13 @@ impl<'n> Engine<'n> {
             oauth2::InstalledFlowReturnMethod::HTTPRedirect,
         ).persist_tokens_to_disk(format!("{}/sheets4", config_dir)).build().await.unwrap();
 
-        let client = hyper::Client::builder().build(hyper_rustls::HttpsConnector::with_native_roots());
+        let client = hyper::Client::builder().build(
+            hyper_rustls::HttpsConnectorBuilder::new().with_native_roots()
+                .https_or_http()
+                .enable_http1()
+                .enable_http2()
+                .build()
+	);
         let engine = Engine {
             opt: opt,
             hub: api::Sheets::new(client, auth),
@@ -2137,7 +2143,7 @@ async fn main() {
     
     let mut app = App::new("sheets4")
            .author("Sebastian Thiel <byronimo@gmail.com>")
-           .version("3.0.0+20220221")
+           .version("3.1.0+20220221")
            .about("Reads and writes Google Sheets.")
            .after_help("All documentation details can be found at http://byron.github.io/google-apis-rs/google_sheets4_cli")
            .arg(Arg::with_name("url")
